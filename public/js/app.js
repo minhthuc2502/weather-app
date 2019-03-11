fbd6ba034c1e6b03e22860e7d0d6f34e01ef4a03
Thuc Pham <minhthuc2502@gmail.com>
2019-03-11 11:39:42 +0100

    remove some unnecessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log("This file js is showed in client side");
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector("input")
 const messageOne = document.querySelector("#message-1")
